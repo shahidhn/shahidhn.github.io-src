ed273c21035ffac6b04e1c1ecfba67e1fbf39f45
shahidhn <shahidhn.nowshad@gmail.com>
2017-07-31 23:29:03 -0500

    Added blog index to linkroll

--- pelicanconf.py
+++ pelicanconf.py
@@ -36,8 +36,9 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
+#        ('Python.org', 'http://python.org/'),
+#        ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('Blog', 'blog_index.html'),
          ('You can modify those links in your config file', '#'),)
 
 # Social widget
